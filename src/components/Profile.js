a2aa9f02117592ca4adf029cb9a6d325b972d8ff
charlie-lyc <ofetwo@gmail.com>
2020-12-02 04:05:53 +0900

    Update validating email

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,29 +1,34 @@
 import { useState } from "react";
 import profile from "../img/profile.png";
+import validator from 'validator';
 
 function Profile() {
     const [ state, setState ] = useState({
         email: null,
-        isShown: false
+        isShown: false,
     });
     function handleChange(event) {
         setState({ email: event.target.value });
     }
     function handleSubmit(event) {
         event.preventDefault();
-        setState({ email: state.email, isShown: !state.isShown });
+        if (validator.isEmail(state.email)) {
+            setState({ email: state.email, isShown: !state.isShown });
+        }
     }
     function handleClick() {
         setState({ isShown: !state.isShown });
     }
     return (     
         <div className="container">
+            {}
             <div className="submit">
             { !state.isShown 
                 ?   (<form onSubmit={handleSubmit}>
                         <label className="leave-meassage">
                             Please leave your email, then I'll send you my profile.<br /><br />
-                        <input type="text" onChange={handleChange} value={state.email} placeHolder="foo@bar.com" />
+                            Only an available email is allowed.<br /><br />
+                        <input onChange={handleChange} name="email" value={state.email} type="text" placeHolder="foo@bar.com" />
                         </label>
                         <button type="submit">Submit</button>
                     </form>)
